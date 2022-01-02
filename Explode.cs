4566e0ff80049569937cc73b47c2ba1b8f34c66e
AyuBBlack <ayub.gadiev95@gmail.com>
2022-01-02 15:53:48 +0300

    0.9.2

--- Explode.cs
+++ Explode.cs
@@ -4,21 +4,21 @@ using System.Collections;
 public class Explode : MonoBehaviour
 {
     public GameObject restartButton;
-    private bool _collisionSet; //Переменная для проверки корректной работы скрипта
-    private void OnCollisionEnter(Collision collision)//Функция срабатывает при соприкосновение с объектом.
+    private bool _collisionSet;                                                //Переменная для проверки корректной работы скрипта
+    private void OnCollisionEnter(Collision collision)                        //Функция срабатывает при соприкосновение с объектом.
     { 
         if(collision.gameObject.tag == "Cube" && !_collisionSet)            //Если объект имеет тэг Cube
         {
             for (int i = collision.transform.childCount - 1; i >= 0; i--) //Перебираем в цикле все дочерние объекты
             {
-                Transform child = collision.transform.GetChild(i); // Выбираем дочерний элемент по индексу
-                child.gameObject.AddComponent<Rigidbody>();       // Добавляем физику для дочерних элементов
+                Transform child = collision.transform.GetChild(i);      // Выбираем дочерний элемент по индексу
+                child.gameObject.AddComponent<Rigidbody>();            // Добавляем физику для дочерних элементов
                 child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(70f, Vector3.up, 5f); //Добавляем взрывную силу
-                child.SetParent (null);                         // Убираем родителя для каждого объекта
+                child.SetParent (null);                              // Убираем родителя для каждого объекта
             }
-            restartButton.SetActive(true);                            //Делаем кнопку рестарт активной
+            restartButton.SetActive(true);                         //Делаем кнопку рестарт активной
             Camera.main.transform.position -= new Vector3(0, 0, 3f) * Time.deltaTime; //Отдаляем камеру
-            Destroy(collision.gameObject);                          //Удаляем all cubes
+            Destroy(collision.gameObject);                       //Удаляем all cubes
             _collisionSet = true;
         }
     }
