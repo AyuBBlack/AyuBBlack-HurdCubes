b34612baea42b791f70479e05e6b75daeecbaadb
AyuBBlack <ayub.gadiev95@gmail.com>
2022-01-02 15:57:47 +0300

    0.9.4

--- Explode.cs
+++ Explode.cs
@@ -11,10 +11,10 @@ public class Explode : MonoBehaviour
         {
             for (int i = collision.transform.childCount - 1; i >= 0; i--) //Перебираем в цикле все дочерние объекты
             {
-                Transform child = collision.transform.GetChild(i);      // Выбираем дочерний элемент по индексу
-                child.gameObject.AddComponent<Rigidbody>();            // Добавляем физику для дочерних элементов
+                Transform child = collision.transform.GetChild(i);      //Выбираем дочерний элемент по индексу
+                child.gameObject.AddComponent<Rigidbody>();            //Добавляем физику для дочерних элементов
                 child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(70f, Vector3.up, 5f); //Добавляем взрывную силу
-                child.SetParent (null);                              // Убираем родителя для каждого объекта
+                child.SetParent (null);                              //Убираем родителя для каждого объекта
             }
             restartButton.SetActive(true);                                             //Делаем кнопку рестарт активной
             Camera.main.transform.position -= new Vector3(0, 0, 3f) * Time.deltaTime; //Отдаляем камеру
